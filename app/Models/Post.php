3a7466358d60b0c3e3b9ba4e9df09eb404a4c8e1
Shrijan Maharjan <shrijanm29@gmail.com>
2025-08-31 11:14:03 +0545

    Implement models and migrations for posts, comments, categories, and their relationships

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -7,4 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     //
+    protected $fillable = ['user_id', 'title', 'body', 'status'];
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class, 'post_category');
+    }
 }
--- database/migrations/2025_08_29_021327_create_posts_table.php
+++ database/migrations/2025_08_29_021327_create_posts_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('title');
+            $table->text('body');
+            $table->string('status')->default('pending');
             $table->timestamps();
         });
     }
